da6d8093a1380d057e1582870d6b03fbe80f568c
dubeyji10 <abhishekdubey806@gmail.com>
2020-03-22 13:10:50 +0530

    added lyrics page functionality

---
+++ music/static/music/js1.js
@@ -0,0 +1,6 @@
+document.getElementById("startOrStopImg").onclick = function() {
+  var audio = document.getElementById("audio");
+  if (audio.paused) audio.play();
+  else audio.pause();
+};
+//  Freepik
